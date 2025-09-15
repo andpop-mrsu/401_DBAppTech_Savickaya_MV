eef10368e0f901b71e482e2c48425cc99c6f35b9
Savitskaya Margarita <savitskayari@yandex.ru>
2025-09-15 18:08:34 +0300

    Task02

---
+++ Task02/tic-tac-toe/src/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SavickayaM\TicTacToe\Controller;
+
+use SavickayaM\TicTacToe\View;
+
+function startGame() {
+    View\startScreen();
+}
---
+++ Task02/tic-tac-toe/src/View.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace SavickayaM\TicTacToe\View;
+
+function startScreen() {
+    \cli\line("Welcome to tic-tac-toe!");
+}
